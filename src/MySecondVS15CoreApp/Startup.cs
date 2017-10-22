2a696dffcd7193ae2a4c74f46a0782b644d294ff
Steven L Speidel <slspeidel@comcast.net>
2017-10-22 08:41:08 -0600

    Initial commit

--- src/MySecondVS15CoreApp/Startup.cs
+++ src/MySecondVS15CoreApp/Startup.cs
@@ -65,7 +65,7 @@ namespace MySecondVS15CoreApp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Main}/{action=Index}/{id?}");
             });
         }
     }
---
+++ src/MySecondVS15CoreApp/gulpfile.js
@@ -0,0 +1,11 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp
+plugins.
+Click her to learn more: http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp");
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
